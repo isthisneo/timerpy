c52e650329ed8ca3229791dfb2dd042e4995b69c
isthisneo <101419016+isthisneo@users.noreply.github.com>
2022-03-11 19:54:27 +0300

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from time import *
+import datetime
+
+root =Tk()
+root.title('Timer')
+
+def clock():
+    text = strftime('%H:%M:%S')
+    label.config(text=text)
+    label.after(1000,clock)
+
+label = Label (root,font=("ds-digital",100),background ="black",foreground="white")
+label.pack(anchor="center") 
+
+clock()
+
+mainloop()
